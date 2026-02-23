dc589900429edade64ddf2c545b3dce5cc0e837c
PranjalSabhaya <sabhayapranjal@gmail.com>
2026-02-23 12:21:34 +0530

    Added middleware in API

--- app/main.py
+++ app/main.py
@@ -1,11 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from app.api.routes import router
+import time
 
 app = FastAPI(
     title="Sales Forecasting API",
     version="1.0.1"
 )
 
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+
+    start_time = time.time()
+
+    response = await call_next(request)
+
+    process_time = round(time.time() - start_time, 4)
+
+    print(
+        f"{request.method} {request.url.path} "
+        f"Status: {response.status_code} "
+        f"Time: {process_time}s"
+    )
+
+    return response
+
+    
 app.include_router(router)
 
 
